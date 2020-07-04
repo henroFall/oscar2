9e90908800105855e69ebdd1935da106fd3cc09b
henroFall <henryfell@gmail.com>
2020-07-04 10:46:36 -0500

    fixed grocery-household lists in alternate locations

--- conky/conky.sh
+++ conky/conky.sh
@@ -2,7 +2,7 @@
 sleep 5
 conky -q -c ~/Conky/conkyrc1 & # This one is the sidebar with the clock and stats
 sleep 2
-conky -q -c ~/Conky/conkyrc2 & # This one has the python scripts in it
+#conky -q -c ~/Conky/conkyrc2 & # This one has the python scripts in it
 #sleep 2
 conky -q -c ~/Conky/conkyrc3 & # This one is the Housewares list
 sleep 2 
