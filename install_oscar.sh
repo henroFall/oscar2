684ca8e38183b857246201216de9bd71250cb495
henroFall <henryfell@gmail.com>
2021-02-14 22:08:00 -0600

    Update install_oscar.sh

--- install_oscar.sh
+++ install_oscar.sh
@@ -25,30 +25,26 @@ check_exit_status() {
 
 function killService() {
     service=$1
-    sudo systemctl stop $service
-    sudo systemctl kill --kill-who=all $service
-
-    # Wait until the status of the service is either exited or killed.
-    while ! (sudo systemctl status "$service" | grep -q "Main.*code=\(exited\|killed\)")
-    do
-        sleep 10
-    done
+    systemctl stop $service
+    systemctl kill --kill-who=all $service
 }
 
 function disableTimers() {
-    sudo systemctl disable apt-daily.timer
-    sudo systemctl disable apt-daily-upgrade.timer
+    systemctl disable apt-daily.timer
+    systemctl disable apt-daily-upgrade.timer
 }
 
 function enableTimers() {
-    sudo systemctl enable apt-daily.timer
-    sudo systemctl enable apt-daily-upgrade.timer
+    systemctl enable apt-daily.timer
+    systemctl enable apt-daily-upgrade.timer
 }
 
 function killServices() {
     killService unattended-upgrades.service
     killService apt-daily.service
     killService apt-daily-upgrade.service
+	echo "Stopped autoupdate services to avoid conflicts... Stand by..."
+	sleep 5
 }
 
 maximize_vert() {
